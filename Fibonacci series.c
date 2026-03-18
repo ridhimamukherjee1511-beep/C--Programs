2c624e5a418aeb2d4e72232d4680b3d1c045c26c
ridhimamukherjee1511-beep <ridhimamukherjee1511@gmail.com>
2026-03-18 13:03:42 +0530

    Add files via upload

---
+++ Fibonacci series.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int n,f1=0,f2=1,f,i;
+    printf("Enter the no");
+    scanf("%d",&n);
+    for(i=3;i<=n;i++){
+        f=f1+f2;
+        printf("%d",f);
+        f1=f2;
+        f2=f;
+    }
+    return 0;
+}
