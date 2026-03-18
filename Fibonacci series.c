d8640ab101d1b02edc7fbd3f6243a362714468e9
ridhimamukherjee1511-beep <ridhimamukherjee1511@gmail.com>
2026-03-18 13:11:45 +0530

    Add initial Fibonacci numbers to output
    Print the first two Fibonacci numbers before the loop.

--- Fibonacci series.c
+++ Fibonacci series.c
@@ -4,9 +4,10 @@ int main(){
     int n,f1=0,f2=1,f,i;
     printf("Enter the no");
     scanf("%d",&n);
+    printf("%d %d ",f1,f2);
     for(i=3;i<=n;i++){
         f=f1+f2;
-        printf("%d",f);
+        printf("%d ",f);
         f1=f2;
         f2=f;
     }
